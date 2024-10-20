8d7f015357036f5c4d34400e6f4d51302fef40ef
viumiuu <viumiu4@gmail.com>
2024-10-20 13:18:14 +0300

    Update start.py

--- start.py
+++ start.py
@@ -3,29 +3,55 @@ from time import sleep
 from PyQt5.QtWidgets import QApplication
  
 app = QApplication([])
-
+ 
 from main_window import *
+ 
+ 
+class Question ():
+    def __init__(self, question, answer, wrong_answer1, wrong_answer2, wrong_answer3):
+        self.question = question
+        self.answer = answer
+        self.wrong_answer1 = wrong_answer1
+        self.wrong_answer2 = wrong_answer2
+        self.wrong_answer3 = wrong_answer3
+        self.isAsking = True 
+        self.count_ask = 0
+        self.count_right = 0
 
-question = "Apple"
+    def got_right(self):
+        self.count_ask += 1
+        self.count_right += 1
 
-answer = "apple"
+    def got_wrong(self):
+        self.count_ask += 1
 
-wrong_answer1 = "orenge"
-wrong_answer2 = "orenge"
-wrong_answer3 = "orenge"
+q1 = Question("3 * 6", "18", "16", "5", "15")
+q2 = Question("3 * 9", "27", "21", "18", "24")
+q3 = Question("6 * 6", "36", "24", "32", "8")
+q4 = Question("9 * 6", "54", "76", "18", "9")
+q5 = Question("9 * 9", "81", "16", "72", "98")
 
+questions = [q1, q2, q3, q4, q5]
+ 
+# Помістили радіо перемикачі в список
 radio_buttons = [rb_ans1, rb_ans2, rb_ans3, rb_ans4]
-shuffle(radio_buttons)
+# перемішали список рандомно
 
+ 
 # підставляємо питання та відповідді до радіо перемикачів
 def new_question():
-    lb_question.setText(question)
-    lb_right_answer.setText(answer)
- 
-    radio_buttons[0].setText(answer)
-    radio_buttons[1].setText(wrong_answer1)
-    radio_buttons[2].setText(wrong_answer2)
-    radio_buttons[3].setText(wrong_answer3)
+    global current_question
+    current_question = choice(questions)
+
+    lb_question.setText(current_question.question)
+    lb_right_answer.setText(current_question.answer)
+
+    shuffle(radio_buttons)
+
+    radio_buttons[0].setText(current_question.answer)
+    radio_buttons[1].setText(current_question.wrong_answer1)
+    radio_buttons[2].setText(current_question.wrong_answer2)
+    radio_buttons[3].setText(current_question.wrong_answer3)
  
 # запускаємо функцію
 new_question()
@@ -42,6 +68,7 @@ def check():
  
             # перевіряємо текст перемикача з правильною відповіддю
             if answer.text() == lb_right_answer.text():
+                current_question.got_right()
                 lb_result.setText('Вірно!')
                 break
  
@@ -50,3 +77,40 @@ def check():
     else:
         # якщо в циклі немає істини (true), обрана не вірна відповідь
         lb_result.setText('Не вірно!')
+        current_question.got_wrong
+ 
+    RadioGroup.setExclusive(True)
+ 
+# Клік на кнопку "Відповісти" або "Наступне запитання"
+def click_ok():
+ 
+    # Якщо користувач натиснув на кнопку "Відповісти"
+    # викликаємо функцію check, щоб перевірити правильну відповідь
+    # та приховуємо группу з питаннями
+    # показуємо групу з відповіддями
+    if btn_next.text() == 'Відповісти':
+        check()
+        gb_question.hide()
+        gb_answer.show()
+ 
+        # Змінюємо текст кнопки "Відповісти" на "Наступне запитання"
+        btn_next.setText('Наступне запитання')
+    else:
+        # Натиснута кнопка "Наступне запитання" то запитуємо нове запитання
+        # приховуємо відповідді 
+        # показуємо групу з питаннями
+        new_question()
+        gb_question.show()
+        gb_answer.hide()
+ 
+        # Змінюємо текст кнопки "Наступне запитання" на "Відповісти" 
+        btn_next.setText('Відповісти')
+ 
+# Під'єднуємо кнопку до обробника функції click_ok()
+btn_next.clicked.connect(click_ok)
+ 
+ 
+# показати вікно
+window.show()
+# запустити додаток
+app.exec_()
