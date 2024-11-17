720f8fb2ba884ea871c65b4a68c802b58e794598
viumiuu <viumiu4@gmail.com>
2024-11-17 13:20:14 +0200

    Add files via upload

---
+++ pyqt/main.py
@@ -0,0 +1,24 @@
+
+
+import random
+
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from ui import Ui_MainWindow
+
+
+
+
+class Widget(QMainWindow):
+   def __init__(self):
+       super().__init__()
+       self.ui = Ui_MainWindow()
+       self.ui.setupUi(self)
+       self.ui.btn_generete.clicked.connect(self.example)
+
+
+app = QApplication([])
+ex = Widget()
+ex.show()
+app.exec_()
